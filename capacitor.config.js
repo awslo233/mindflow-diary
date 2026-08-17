b2b77ca9debd3bcfebc9d1c491926d4708769c78
awslo233 <846852845@qq.com>
2026-08-17 16:20:47 +0800

    Remove comment about local packaging mode

--- capacitor.config.js
+++ capacitor.config.js
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.mindflow.diary',
   appName: '心流日记',
   webDir: 'www',
-  // 本地打包模式：加载 www/ 下的静态文件，不再依赖 Coze 服务器
   server: {
     androidScheme: 'https',
     cleartext: false,
